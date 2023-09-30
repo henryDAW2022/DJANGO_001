c6db72ed065dc8977632e725159a58bb64551bda
Henry <hvilmur686@g.educaand.es>
2023-09-30 18:42:35 +0200

    primer commit creacion del proyecto

---
+++ envirt/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\Randstad\Desktop\Udemy\Python\django\prod\envirt\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
